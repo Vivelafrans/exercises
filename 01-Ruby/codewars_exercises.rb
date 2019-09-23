1990b07ae19e4644a7c715c48f2e365e7c73223c
Frans Knaapen <fransknaapen@gmail.com>
2019-09-23 11:40:18 +0200

    new exercises finished

--- 01-Ruby/codewars_exercises.rb
+++ 01-Ruby/codewars_exercises.rb
@@ -1,6 +1,16 @@
 # My attempts on Codewars
 
-# Find numbers which are divisible by given number
+# Find numbers which are divisible by given number (8kyu)
 def divisible_by(numbers, divisor)
   numbers.select { |n| (n % divisor).zero? }
+end
+
+# Sort string by number represented in each word (6kyu)
+def order(words)
+  words.split.sort_by { |x| x[/\d+/].to_i }.join(' ')
+end
+
+# Find difference in volume regardless which entry is the highest (8 kyu)
+def find_difference(first, second)
+  (first.inject(:*) - second.inject(:*)).abs
 end
